cee030461ae2a09834eae056ef463d9d2d15de37
vivek khedkar <vivek.khedkar@gmail.com>
2022-04-28 22:08:39 -0400

    after fixing the issue with touchscreen tap on work dropdown

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
     const navMenuLink = document.querySelector('.nav-menu-link');
+    const navMenuWork = document.querySelector('.nav-menu-work');
     // const navMenuWork = document.querySelector('.nav-menu-work');
     const navMenuWorkDropdown = document.querySelector('.nav-menu-work-dropdown');
     
@@ -11,21 +12,38 @@ document.addEventListener('DOMContentLoaded', function(){
     const b_logo = document.querySelector('.b-logo');
     const containerHeader = document.querySelector('.container-header');
 
-    const thirtysixDaysDiv1Img = document.querySelector('.thirtysix-days-div1-img');
-    const thirtysixDaysDiv2Img = document.querySelector('.thirtysix-days-div2-img');
+    const thirtysixDaysDiv1Img = document.querySelector('.thirtysix-days-div1');
+    const thirtysixDaysDiv2Img = document.querySelector('.thirtysix-days-div2');
 
     
 
     
 
-    // This makes nav-menu-work-div to disappear when clicked outside of the menu
+    
+
+    // This makes nav-menu-work-div to disappear when clicked outside of the menu only if screen width is greater than 770px
+
+    if (screen.width > 770) 
     document.addEventListener('click', e =>{
         
         navMenuWorkDropdown.classList.remove('active');
         navMenuDiv2.classList.remove('active');
         workbar.classList.remove('active');
-        
-        
+          
+    })
+
+    // navMenuLink.addEventListener("touchstart", e =>{
+    //     console.log(e);
+    //     navMenuWorkDropdown.classList.toggle('active');
+    //     navMenuDiv2.classList.toggle('active');
+    //     workbar.classList.toggle('active'); 
+    // })
+
+    navMenuLink.addEventListener("touchend", e =>{
+        console.log(e);
+        navMenuWorkDropdown.classList.toggle('active');
+        navMenuDiv2.classList.toggle('active');
+        workbar.classList.toggle('active'); 
     })
 
     hamburger.addEventListener("click",()=>{
@@ -35,14 +53,7 @@ document.addEventListener('DOMContentLoaded', function(){
         
     })
 
-    navMenuLink.addEventListener("click",()=>{
-        console.log('navMenuLink clicked');
-        
-        navMenuWorkDropdown.classList.toggle('active');
-        navMenuDiv2.classList.toggle('active');
-        workbar.classList.toggle('active');
-
-    })
+   
 
     navMenuLink.addEventListener("mouseover", ()=>{
         console.log('navMenuLink mouseover');
@@ -51,13 +62,11 @@ document.addEventListener('DOMContentLoaded', function(){
         workbar.classList.toggle('active'); 
     })
 
-
-    navMenuLink.addEventListener("click", ()=>{
-        console.log('navMenuLink clicked');
-    })
-
-    // navMenuWork.addEventListener("mouseover",()=>{
-    //     console.log('navMenuWork mouseover');
+    // navMenuLink.addEventListener("click", ()=>{
+    //     console.log('navMenuLink clicked');
+    //     navMenuWorkDropdown.classList.toggle('active');
+    //     navMenuDiv2.classList.toggle('active');
+    //     workbar.classList.toggle('active'); 
     // })
 
 
@@ -67,33 +76,25 @@ document.addEventListener('DOMContentLoaded', function(){
         if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
             console.log("scrolled 500 pixel");
             signature_logo.classList.add('active');
-            // containerHeader.classList.add('active');
-            // b_logo.classList.add('active');
+            
         }
         else {
             signature_logo.classList.remove('active');
-            // containerHeader.classList.remove('active');
-            // b_logo.classList.remove('active');
+            
         }    
     }
 
     thirtysixDaysDiv1Img.addEventListener("click",()=>{
         console.log('thirtysixDaysImg clicked!');
-        // window.location('thirtysix-days-2018.html');
+        
         location.href='thirtysix-days-2018.html';
     })
 
     thirtysixDaysDiv2Img.addEventListener("click",()=>{
         console.log('thirtysixDaysImg clicked!');
-        // window.location('thirtysix-days-2018.html');
+        
         location.href='thirtysix-days-2020.html';
     })
-
-    // swiper initialize script
-    
-      
-
-
 
 });
 
